c6b0a20d97cfe841aebd5da3fe0b10b0e7179b31
Swapnil Gaikwad <swapnilg@thoughtworks.com>
2016-09-25 10:21:43 +0530

    Added Diameter implementation.

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,5 +1,6 @@
 class BinaryTree {
     Node root;
+    private int diameter = 0;
 
     int count() {
         return count(root);
@@ -8,12 +9,38 @@ class BinaryTree {
     private int count(Node root) {
         if (root == null) {
             return 0;
-        }
-        else {
+        } else {
             int leftCount = count(root.left);
             int rightCount = count(root.right);
             return 1 + leftCount + rightCount;
         }
     }
 
+    int diameter() {
+        diameter = 0;
+        return diameter(root);
+    }
+
+    private int diameter(Node root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = height(root.left);
+            int rightHeight = height(root.right);
+
+            int leftDiameter = diameter(root.left);
+            int rightDiameter = diameter(root.right);
+            int maxDiameter = Math.max(leftDiameter , rightDiameter);
+            return Math.max(maxDiameter, 1 + leftHeight + rightHeight);
+        }
+    }
+
+    private int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        else {
+            return 1 + Math.max(height(root.left) , height(root.right));
+        }
+    }
 }
--- test/TreeTest.java
+++ test/TreeTest.java
@@ -1,36 +1,71 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TreeTest {
     private int[] output;
     private int i = 0;
-    @Test
-    public void shouldCreateTreeStructure() {
-        BinaryTree tree = new BinaryTree();
+    private BinaryTree tree = new BinaryTree();
 
+    @Before
+    public void setUp() {
         tree.root = new Node(1);
-
         tree.root.left = new Node(2);
         tree.root.right = new Node(3);
-
         tree.root.left.right = new Node(4);
         tree.root.right.left = new Node(5);
 
         output = new int[tree.count()];
         i = 0;
-        inorder(tree.root);
+    }
 
+    @Test
+    public void shouldCreateTreeStructure() {
+        getTreeInArray(tree.root);
         int[] expectedArray = {2,4,1,5,3};
-
         Assert.assertArrayEquals(expectedArray , output);
     }
 
-    private void inorder(Node root) {
+
+    @Test
+    public void shouldReturnDiameterOfTree() {
+        int diameter = tree.diameter();
+
+        Assert.assertEquals(5 , diameter);
+    }
+
+
+    @Test
+    public void shouldReturnDiameterIfLongestPathExistInLeftSideIfTree() {
+        Node right = tree.root.right;
+        tree.root.right = null;
+
+        tree.root.left.left = new Node(6);
+        tree.root.left.left.left = new Node(7);
+
+        tree.root.left.right.left = new Node(8);
+        tree.root.left.right.left.left = new Node(9);
+        tree.root.left.right.left.left.left = new Node(10);
+
+        int diameter = tree.diameter();
+
+        Assert.assertEquals(7 , diameter);
+
+        tree.root.right = right;
+        tree.root.left.left = null;
+        tree.root.left.right.left = null;
+    }
+
+    /**
+     * set output array to inorder traversal of tree.
+     * @param root of tree.
+     */
+    private void getTreeInArray(Node root) {
         if (root != null) {
-            inorder(root.left);
+            getTreeInArray(root.left);
             System.out.println("Data : " + root.getData());
             output[i++] = root.getData();
-            inorder(root.right);
+            getTreeInArray(root.right);
         }
     }
 }
